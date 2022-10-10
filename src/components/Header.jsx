aedfe3ebba118caab6c01094646584169b127769
tahsinkarim <tahsinnkarim@gmail.com>
2022-10-10 23:09:57 +0600

    Added Home page image and title

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,19 +10,19 @@ const Header = () => {
             <Link className='btn btn-ghost normal-case text-xl'>Quizlet</Link>
           </div>
           <div className='flex-none'>
-            <ul className='menu menu-horizontal text-sm p-0 '>
+            <ul className='menu menu-horizontal text-sm p-0 sm:text-base'>
               <li>
-                <Link className='px-1' to='/'>
+                <Link className='px-2 sm:px-5' to='/'>
                   Home
                 </Link>
               </li>
               <li>
-                <Link className='px-1' to='/statistics'>
+                <Link className='px-2 sm:px-5' to='/statistics'>
                   Statistics
                 </Link>
               </li>
               <li>
-                <Link className='px-1' to='/blog'>
+                <Link className='px-2 sm:px-5' to='/blog'>
                   Blog
                 </Link>
               </li>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,14 +1,32 @@
 import React from "react";
 import { useLoaderData } from "react-router-dom";
+import img from "../components/images/hero.jpg";
 import QuizCards from "./QuizCards";
 
 const Home = () => {
   const { data } = useLoaderData();
   return (
-    <div className='grid md:grid-cols-2 lg:grid-cols-4 max-w-6xl mx-auto '>
-      {data.map((quiz) => (
-        <QuizCards key={quiz.id} quiz={quiz}></QuizCards>
-      ))}
+    <div>
+      <div className='grid md:grid-cols-2 max-w-6xl mx-auto justify-center items-center'>
+        <div className='mx-auto md:order-last'>
+          <img src={img} alt='' />
+        </div>
+        <div className='mx-auto text-center md:text-left md:pl:10'>
+          <h2 className='text-4xl font-bold text-primary'>
+            Best Online Quizzes
+          </h2>
+          <p className='mt-4 max-w-xs text-gray-500'>
+            Keeping yourself entertained and educated is just a quiz away.our
+            online quizzes are an excellent source of knowledge on front-end
+            technologies.
+          </p>
+        </div>
+      </div>
+      <div className='grid sm:grid-cols-2 lg:grid-cols-4 max-w-6xl mx-auto '>
+        {data.map((quiz) => (
+          <QuizCards key={quiz.id} quiz={quiz}></QuizCards>
+        ))}
+      </div>
     </div>
   );
 };
